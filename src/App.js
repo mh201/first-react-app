969a2c8088068d11056495b05cec86eb7c04d4ae
Miguel H <miguelherrera1101@gmail.com>
2025-02-15 18:09:25 -0500

    Use of children components and filter of done tasks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React, {Component} from "react";
+import { TodoBanner } from "./TodoBanner";
+import { TodoCreator } from "./TodoCreator";
+import { TodoRow } from "./TodoRow";
+import { VisibilityControl } from "./VisibilityControl";
 
 export default class App extends Component {
 
@@ -10,7 +14,8 @@ export default class App extends Component {
                   {action: "Get Shoes", done: false },
                   {action: "Collect Tickets", done: true },
                   {action: "Call Joe", done: false }],
-      newItemText: ""
+      // newItemText: ""
+      showCompleted: true
     }
   }
 
@@ -18,51 +23,84 @@ export default class App extends Component {
     this.setState({ newItemText: event.target.value})
   }
 
-  createNewTodo = () => {
-    if (!this.state.todoItems
-              .find(item => item.action === this.state.newItemText)
-          && this.state.newItemText !== '') {
-                this.setState({
-                  todoItems: [...this.state.todoItems,
-                              { action:this.state.newItemText, done : false}],
-                  newItemText : ""
-                })
-              }
-  }
+  // createNewTodo = () => {
+  //   if (!this.state.todoItems
+  //             .find(item => item.action === this.state.newItemText)
+  //         && this.state.newItemText !== '') {
+  //               this.setState({
+  //                 todoItems: [...this.state.todoItems,
+  //                             { action:this.state.newItemText, done : false}],
+  //                 newItemText : ""
+  //               })
+  //             }
+  // }
 
+  createNewTodo = (task) => {
+    if (!this.state.todoItems.find(item => item.action === task)
+      && task !== '') {
+      this.setState({
+        todoItems: [...this.state.todoItems, { action: task, done: false}]
+      });
+    }
+  }
   toggleTodo = (todo) => this.setState({ todoItems:
     this.state.todoItems.map(item => item.action === todo.action
       ? { ...item, done:!item.done } : item) }); // Probar poner el item explicito
 
-  todoTableRows = () => this.state.todoItems.map(item =>
-    <tr key={ item.action }>
-      <td>{ item.action }</td>
-      <td>
-        <input type="checkbox" checked={ item.done }
-          onChange={ () => this.toggleTodo(item) } />
-      </td>
-    </tr> );
+  // todoTableRows = () => this.state.todoItems.map(item =>
+  //   <tr key={ item.action }>
+  //     <td>{ item.action }</td>
+  //     <td>
+  //       <input type="checkbox" checked={ item.done }
+  //         onChange={ () => this.toggleTodo(item) } />
+  //     </td>
+  //   </tr> );
+
+  // todoTableRows = () => this.state.todoItems.map(item =>
+  //   <TodoRow key={item.action} item={item} callback={this.toggleTodo} />)
+  
+  todoTableRows = (doneValue) => this.state.todoItems
+      .filter(item => item.done === doneValue).map(item =>
+        <TodoRow key={ item.action } item={ item }
+          callback={ this.toggleTodo } />)
 
   render = () => 
       <div>
-        <h4 className="bg-primary text-white text-center p-2">
+        {/* <h4 className="bg-primary text-white text-center p-2">
           { this.state.userName }'s To Do List
           ({ this.state.todoItems.filter(t => !t.done).length } items to do)
-        </h4>
+        </h4>  */}
+        <TodoBanner name={ this.state.userName } tasks={ this.state.todoItems } />
         <div className="container-fluid">
-          <div className="my-1">
+          {/* <div className="my-1">
             <input className="form-control"
                   value={this.state.newItemText}
                   onChange={ this.updateNewTextValue } />
             <button className="btn btn-primary mt-1" 
                   onClick={ this.createNewTodo }>Add</button>
-          </div>
+          </div> */}
+          <TodoCreator callback={ this.createNewTodo } />
           <table className="table table-striped table-bordered">
             <thead>
               <tr><th>Description</th><th>Done</th></tr>
             </thead>
-            <tbody>{ this.todoTableRows() }</tbody>
+            <tbody>{ this.todoTableRows(false) }</tbody>
           </table>
+          <div className="bg-secondary text-white text-center p-2">
+            <VisibilityControl description="Completed Tasks"
+              isChecked={this.state.showCompleted}
+              callback={ (checked) =>
+                this.setState({ showCompleted: checked})} />
+          </div>
+
+          { this.state.showCompleted && 
+            <table className="table table-striped table-bordered">
+              <thead>
+                <tr><th>Description</th><th>Done</th></tr>
+              </thead>
+              <tbody>{ this.todoTableRows(true) }</tbody>
+            </table>
+          }
         </div>
       </div>
     
